46abe9854bdd4c963e074304404aa15144d607c6
Vo Minh Tri <vo.minh-tri1@hdsaison.com.vn>
2025-02-14 09:14:36 +0700

    change image logo

--- dist/v-1739499133954/js/app-loader.js
+++ dist/v-1739499133954/js/app-loader.js
@@ -10,7 +10,7 @@
 (function() {
   var loadApp, loadJS, loadPlugin, loadPlugins, loadStylesheet, mainLoad, promise;
 
-  window._version = "v-1739495386691";
+  window._version = "v-1739499133954";
 
   window.taigaConfig = {
     "api": "http://localhost:8000/api/v1/",
